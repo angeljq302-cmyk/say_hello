8886056a1ab7ceb9aea0ed2d01681163d1b2aad6
Angel J. Quezada <angeljq302@gmail.com>
2026-01-30 09:55:32 -0600

    Mi primer saludo

---
+++ hello.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Esta función despliega el saludo de bienvenida
+        Saludar();
+    }
+
+    static void Saludar()
+    {
+        Console.WriteLine("¡Bienvenido al curso de programación!");
+    }
+}
